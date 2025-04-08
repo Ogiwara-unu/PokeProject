dc87a628624ff3df5fef80f99eafc323c5a7c214
Chris <chrischavezvilla@gmail.com>
2025-04-08 11:03:10 -0600

    ThirdCommit

--- frontend/pokeProjectFE/src/App.tsx
+++ frontend/pokeProjectFE/src/App.tsx
@@ -7,6 +7,11 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound/NotFound';
 import PokeSearch from './pages/PokeSearch/PokeSearch';
 import PokemonDetailCard from './components/Poke/PokemonDetailCard';
+<<<<<<< Updated upstream
+=======
+import Favorites from './pages/favorites/favorites';
+import PokeCompare from './pages/PokeCompare/PokeCompare';
+>>>>>>> Stashed changes
 
 
 /* Core CSS required for Ionic components to work properly */
@@ -43,6 +48,7 @@ setupIonicReact();
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
+<<<<<<< Updated upstream
       <IonRouterOutlet>
         <Route exact path="/home">
           <Home />
@@ -58,6 +64,31 @@ const App: React.FC = () => (
         </Route>
         <Route path="/details/:name" component={PokemonDetailCard} exact />
       </IonRouterOutlet>
+=======
+    <IonRouterOutlet>
+  <Switch>
+    <Route exact path="/home">
+      <Home />
+    </Route>
+    <Route exact path="/">
+      <Redirect to="/home" />
+    </Route>
+    <Route exact path="/search">
+      <PokeSearch />
+    </Route>
+    <Route exact path="/details/:name" component={PokemonDetailCard} />
+    <Route exact path="/favorites">
+      <Favorites />
+    </Route>
+    <Route exact path="/compare">
+    <PokeCompare/>
+    </Route>
+    <Route path="*">
+      <NotFound />
+    </Route>
+  </Switch>
+</IonRouterOutlet>
+>>>>>>> Stashed changes
     </IonReactRouter>
   </IonApp>
 );
